9050d855d708eea122095401193919a0f28a3a7f
titan2903 <titanioy98@gmail.com>
2020-04-03 03:41:38 +0700

    add API IMDB by ImdbId

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,9 @@ const { Router } = require('express')
 const router = Router()
 const API = require('./routeAPI.js')
 const Users = require('./routeUsers.js')
+const ImdbId = require('./routeApiIMDBId');
 
+router.use('/imdbid', ImdbId)
 router.use('/api', API)
 router.use('/users', Users)
 
