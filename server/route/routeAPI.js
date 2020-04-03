2c608f154dbf4b3f0f98ded5151b2f8895b7397a
Jessica_Sugianto <jezicalove12@gmail.com>
2020-04-03 10:02:04 +0700

    berhasil mengambil data dari 3rd API google search dan menampilkan pada halaman client

--- server/route/routeAPI.js
+++ server/route/routeAPI.js
@@ -2,6 +2,6 @@ const { Router } = require('express')
 const router = Router()
 const Controller = require('../controllers/api_controllers.js')
 
-
+router.get('/google_search/:search', Controller.googleSearch)
 
 module.exports = router
