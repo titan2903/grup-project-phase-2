ec036b3000844df19c67b7954217396b61cc7ffc
Jessica_Sugianto <jezicalove12@gmail.com>
2020-04-03 13:26:59 +0700

    memperbaiki tampilan

--- client/asset/script/main.js
+++ client/asset/script/main.js
@@ -1,25 +1,31 @@
-
 $(document).ready(() => {
     console.log(localStorage.getItem('token'))
-    if (!localStorage.getItem('token') || localStorage.getItem('token') === '') {
+    if (!localStorage.getItem('token') || localStorage.getItem('token') === null) {
         $('#link_search').hide()
         $('#link_search_google').hide()
+        $('#link_logout').hide()
+        $('#link_holiday').hide()
         $('#register').hide()
         $('#search_movie').hide()
         $('#movie-list').hide()
         $('#search_google').hide()
         $('#search_list').hide()
+        $('#holiday').hide()
+        $('#login').show()
     } else {
         $('#link_search').show()
         $('#link_search_google').show()
+        $('#link_holiday').show()
         $('#link_register').hide()
         $('#link_login').hide()
+        $('#link_logout').show()
         $('#register').hide()
         $('#login').hide()
         $('#search_movie').show()
         $('#movie-list').show()
         $('#search_google').hide()
         $('#search_list').hide()
+        $('#holiday').hide()
     }
 
     register = () => {
@@ -29,6 +35,7 @@ $(document).ready(() => {
         $('#movie-list').hide()
         $('#search_google').hide()
         $('#search_list').hide()
+        $('#holiday').hide()
     }
 
     login = () => {
@@ -38,6 +45,7 @@ $(document).ready(() => {
         $('#movie-list').hide()
         $('#search_google').hide()
         $('#search_list').hide()
+        $('#holiday').hide()
     }
 
     movie = () => {
@@ -47,6 +55,7 @@ $(document).ready(() => {
         $('#movie-list').show()
         $('#search_google').hide()
         $('#search_list').hide()
+        $('#holiday').hide()
     }
 
     google = () => {
@@ -56,6 +65,7 @@ $(document).ready(() => {
             $('#movie-list').hide()
             $('#search_google').show()
             $('#search_list').show()
+            $('#holiday').hide()
         }
         //Regisster
 
@@ -80,6 +90,7 @@ $(document).ready(() => {
                 $('#login').hide()
                 $('#search_movie').show()
                 $('#movie-list').show()
+                $('#holiday').hide()
 
                 $('#success').append(`<div class="alert alert-success" role="alert"> User berhasil ditambhakan </div>`)
             })
@@ -113,6 +124,7 @@ $(document).ready(() => {
                 $('#login').hide()
                 $('#search_movie').show()
                 $('#movie-list').show()
+                $('#holiday').hide()
 
                 localStorage.setItem('token', result.token)
 
@@ -149,38 +161,45 @@ $(document).ready(() => {
                 $('#error').append(`<div class="alert alert-danger" role="alert"> Error login dari server: ${err} </div>`)
             })
     }
-    
-     // AJAX HOLIDAY
-    $.ajax({
-        url: 'http://localhost:3000/api/holidays/ID/2020',
-        method: 'GET',
-        headers: { 
-            token: localStorage.getItem('token')
-        }
-    })
-        .done(holiday => {
-            // console.log(holiday);
-            $('#register').hide()
+
+    // AJAX HOLIDAY
+    holiday = () => {
+
+        $.ajax({
+                url: 'http://localhost:3000/api/holidays/ID/2020',
+                method: 'GET',
+                headers: {
+                    token: localStorage.getItem('token')
+                }
+            })
+            .done(holiday => {
+                // console.log(holiday);
+
+                $('#register').hide()
                 $('#login').hide()
-                $('#search_movie').show()
-                $('#movie-list').show()
-            holiday.forEach(el => {
-                // console.log(el.name);
-                let date = new Date(el.date.iso).toDateString()
-                $('#list_holiday').append(`
-                    <tr>
-                        <td>${date}</td>
-                        <td>${el.name}</td>
-                    </tr>
-                `)
-            });
-        })
-        .catch(err => {
-            console.log(err);
-        })
+                $('#search_movie').hide()
+                $('#movie-list').hide()
+                $('#holiday').show()
+
+                holiday.forEach(el => {
+                    // console.log(el.name);
+                    let date = new Date(el.date.iso).toDateString()
+                    $('#list_holiday').append(`
+                        <tr>
+                            <td>${date}</td>
+                            <td>${el.name}</td>
+                        </tr>
+                    `)
+                });
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
 })
 
 function onSignIn(googleUser) {
+
     var id_token = googleUser.getAuthResponse().id_token;
     $.ajax({
         url: 'http://localhost:3000/users/google-login',
@@ -189,7 +208,7 @@ function onSignIn(googleUser) {
             token: id_token
         },
         statusCode: {
-            200: function(response){
+            200: function(response) {
                 console.log('>> response Signin Google', response.token);
                 localStorage.setItem('token', response.token)
             }
@@ -199,8 +218,23 @@ function onSignIn(googleUser) {
 
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-        console.log('User signed out.');
+    if (auth2) {
+        auth2.signOut().then(function() {
+            console.log('User signed out.');
+
+            $('#link_search').hide()
+            $('#link_search_google').hide()
+            $('#register').hide()
+            $('#search_movie').hide()
+            $('#movie-list').hide()
+            $('#search_google').hide()
+            $('#search_list').hide()
+            $('#holiday').hide()
+            $('#login').show()
+
+            localStorage.removeItem('token')
+        });
+    } else {
         $('#link_search').hide()
         $('#link_search_google').hide()
         $('#register').hide()
@@ -208,6 +242,9 @@ function signOut() {
         $('#movie-list').hide()
         $('#search_google').hide()
         $('#search_list').hide()
-      localStorage.removeItem('token')
-    });
-  }
+        $('#holiday').hide()
+        $('#login').show()
+
+        localStorage.removeItem('token')
+    }
+}
--- server/route/routeAPI.js
+++ server/route/routeAPI.js
@@ -1,10 +1,11 @@
 const { Router } = require('express')
 const router = Router()
 const Controller = require('../controllers/api_controllers.js')
+const Auth = require('../middlewares/auth')
 
-router.get('/holidays/:ISOcountry/:year', Controller.holiday) 
+router.get('/holidays/:ISOcountry/:year', Auth, Controller.holiday)
 
-router.get('/google_search/:search', Controller.googleSearch)
+router.get('/google_search/:search', Auth, Controller.googleSearch)
 
 
 module.exports = router
